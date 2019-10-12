a4a75969a14401e46739d8b59929fb11a35803b5
lzq <670123211@qq.com>
2019-10-12 13:26:19 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,21 @@
+// //index.js
+// //获取应用实例
+const app = getApp()
+const fetch = require('../../utils/fetch')
+Page({
+  data: {
+    slides: [],
+    categories: [],
+  },
+  onLoad: function(options) {
+    fetch('slides').then(res => {
+      this.setData({slides: res.data})
+    })
+
+    fetch('categories').then(res => {
+      this.setData({categories: res.data})
+      console.log(res.data)
+    })
+   
+  }
+})
